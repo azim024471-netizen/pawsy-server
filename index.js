534095a49d8966d239cd561d0cb42b974e83cd92
azim024471-netizen <azim024471@gmail.com>
2026-05-20 12:10:59 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+ 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =process.env.MONGODB_URI;
+const port =process.env.PORT || 1234;
+
+//  const cors = require('cors');
+//  app.use(cors());
+ const cors = require('cors');
+ app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {await client.connect();
+
+    
+const db = client.db('Pawsy');
+const petsCollection =db.collection('allpets');
+
+
+// post ////////////////////////////////////////////////////////////////////////////////
+
+app.post('/allpets', async(req, res)=>{
+    const petsData = req.body;
+    console.log(petsData, 'this is from server consol pets post')
+    const result = await petsCollection.insertOne(petsData);
+     console.log(result, 'inserted result')
+    res.json(result)
+
+})
+
+
+///////get///////////////////
+
+
+app.get('/allpets', async(req, res)=>{
+     const cursor = petsCollection.find();
+  const result = await cursor.toArray();
+  console.log(result, 'result of  get all pets')
+  res.send(result) 
+})
+
+
+app.get('/mypets/:userId', async(req, res) => {
+    const userId = req.params.userId
+    const cursor = petsCollection.find({ ownerId: userId })
+    const result = await cursor.toArray()
+    res.send(result)
+})
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('wellcome to pawsy!')
+})
+
+app.listen(port, () => {
+  console.log(`pawsy server is running ${port}`)
+})
